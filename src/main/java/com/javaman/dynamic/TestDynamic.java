3c957dd713ec8a9a6f0fafdf913790e5c3e4500f
pengzhe <pengzhepost@sina.com>
2019-03-12 22:54:51 +0800

    Spring 扩展自定义标签

--- src/main/java/com/javaman/dynamic/TestDynamic.java
+++ src/main/java/com/javaman/dynamic/TestDynamic.java
@@ -6,7 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * @author pengzhe
  * @date 2019-03-12 22:43
- * @description
+ * @description 在开发产品级组件时, 为了更好的封装组件, 增强组件的易用性, 一般都将组件进行标签化定义(如Dubbo)
+ * Spring中,自定义组件标签非常方便,只需经过以下几个步骤
+ * 1):采用XSD描述自定义标签的元素属性
+ * 2):编写Bean定义的解析器
+ * 3):注册自定义标签解析器
+ * 4):绑定命名空间解析器
  */
 
 public class TestDynamic {
--- src/main/java/com/javaman/dynamic/UserServiceDefinitionParser.java
+++ src/main/java/com/javaman/dynamic/UserServiceDefinitionParser.java
@@ -12,12 +12,14 @@ import org.w3c.dom.Element;
 public class UserServiceDefinitionParser implements BeanDefinitionParser {
 
     public BeanDefinition parse(Element element, ParserContext parserContext) {
+        //通过BeanDefinitionBuilder创建Bean定义
         BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(UserService.class);
+        //获取自定义标签的属性
         String dao = element.getAttribute("dao");
-        beanDefinitionBuilder.addPropertyReference("userDao",dao);
+        beanDefinitionBuilder.addPropertyReference("userDao", dao);
         AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
         //注册BEAN定义
-        parserContext.registerBeanComponent(new BeanComponentDefinition( beanDefinition,"userService"));
+        parserContext.registerBeanComponent(new BeanComponentDefinition(beanDefinition, "userService"));
         return null;
     }
 }
